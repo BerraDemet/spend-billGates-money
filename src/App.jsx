e2595fa92ae0aa28d4252a7f5f09fa7b672f968f
Berra Demet <berrademet24@gmail.com>
2025-01-16 04:42:30 +0300

    build header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import Header from "./assets/components/Header";
 import Footer from "./assets/components/Footer";
 import ProductList from "./assets/components/ProductList";
